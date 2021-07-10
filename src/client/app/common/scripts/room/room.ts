83b929d9766ac17fda1f976423d61bbb72a0ba90
CGsama <CGsama@outlook.com>
2021-07-10 00:49:37 -0400

    change control

--- src/client/app/common/scripts/room/room.ts
+++ src/client/app/common/scripts/room/room.ts
@@ -189,8 +189,8 @@ export class Room {
 		this.controls.maxAzimuthAngle = Math.PI / 2;
 		this.controls.enableDamping = true;
 		this.controls.dampingFactor = 0.2;
-		this.controls.mouseButtons.LEFT = 1;
-		this.controls.mouseButtons.MIDDLE = 2;
+		this.controls.mouseButtons.LEFT = 2;
+		this.controls.mouseButtons.MIDDLE = 1;
 		this.controls.mouseButtons.RIGHT = 0;
 		//#endregion
 
