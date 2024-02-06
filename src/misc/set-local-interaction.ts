441be902a95a7deb79c9de84c13f6d2233c89b96
CGsama <cgsama@outlook.com>
2024-02-06 12:03:56 -0500

    Update set-local-interaction.ts

--- src/misc/set-local-interaction.ts
+++ src/misc/set-local-interaction.ts
@@ -1,16 +1,12 @@
 import { Notes, DriveFiles } from "../models";
-import { apiLogger } from '../services/logger';
 
 export async function setLocalInteraction( noteid: string ) {
-	const note = await Notes.findOneBy({ id: noteid });
+	const note = await Notes.findOne(noteid);
 
-    apiLogger.info(`set local interaction for note: ${noteid}`);
-	
-	await Notes.update({id: noteid}, {localInteraction: true});
+    await Notes.update({id: noteid}, {localInteraction: true});
 
 	for (const fileid of note.fileIds) {
-        apiLogger.info(`set local interaction for file: ${fileid}`);
-		await DriveFiles.update({id: fileid}, {localInteraction: true});
+        await DriveFiles.update(fileid, {localInteraction: true});
 	}
 
 	if(note.replyId != null){
